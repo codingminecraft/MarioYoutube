a37c753da6c3730c4204bb62ace0c5c577f32572
Gabe Ambrosio <gamesbygabe1@gmail.com>
2020-07-28 10:01:55 -0500

    Physics 11: SAT Explained

--- src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
+++ src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
@@ -305,4 +305,101 @@ public class IntersectionDetector2D {
         Vector2f circleToBox = new Vector2f(localCirclePos).sub(closestPointToCircle);
         return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
     }
+
+    // =============================================================================
+    // AABB vs. Primitive tests
+    // =============================================================================
+    public static boolean AABBAndCircle(AABB box, Circle circle) {
+        return circleAndAABB(circle, box);
+    }
+
+    public static boolean AABBAndAABBB(AABB b1, AABB b2) {
+        Vector2f axesToTest[] = {new Vector2f(0, 1), new Vector2f(1, 0)};
+        for (int i=0; i < axesToTest.length; i++) {
+            if (!overlapOnAxis(b1, b2, axesToTest[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean AABBAndBox2D(AABB b1, Box2D b2) {
+        Vector2f axesToTest[] = {
+                new Vector2f(0, 1), new Vector2f(1, 0),
+                new Vector2f(0, 1), new Vector2f(0, 1)
+        };
+        JMath.rotate(axesToTest[2], b2.getRigidbody().getRotation(), b2.getRigidbody().getPosition());
+        JMath.rotate(axesToTest[3], b2.getRigidbody().getRotation(), b2.getRigidbody().getPosition());
+        for (int i=0; i < axesToTest.length; i++) {
+            if (!overlapOnAxis(b1, b2, axesToTest[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // =============================================================================
+    // SAT helpers
+    // =============================================================================
+    private static boolean overlapOnAxis(AABB b1, AABB b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(AABB b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(Box2D b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static Vector2f getInterval(AABB rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f min = rect.getMin();
+        Vector2f max = rect.getMax();
+
+        Vector2f vertices[] = {
+            new Vector2f(min.x, min.y), new Vector2f(min.x, max.y),
+            new Vector2f(max.x, min.y), new Vector2f(max.x, max.y)
+        };
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i=1; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
+    }
+
+    private static Vector2f getInterval(Box2D rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f vertices[] = rect.getVertices();
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i=1; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
+    }
 }
