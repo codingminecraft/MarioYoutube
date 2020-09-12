efa5ef45116403ee3b39eb31f2829515587a5ca5
Gabe Ambrosio <gamesbygabe1@gmail.com>
2020-09-12 10:21:59 -0500

    Physics ep 14: Collision Manifolds

---
+++ src/main/java/physics2d/rigidbody/Collisions.java
@@ -0,0 +1,28 @@
+package physics2d.rigidbody;
+
+import org.joml.Vector2f;
+import physics2d.primitives.Circle;
+
+public class Collisions {
+    public static CollisionManifold findCollisionFeatures(Circle a, Circle b) {
+        CollisionManifold result = new CollisionManifold();
+        float sumRadii = a.getRadius() + b.getRadius();
+        Vector2f distance = new Vector2f(b.getCenter()).sub(a.getCenter());
+        if (distance.lengthSquared() - (sumRadii * sumRadii) > 0) {
+            return result;
+        }
+
+        // Multiply by 0.5 because we want to separate each circle the same
+        // amount. Consider updating to factor in the momentum and velocity
+        float depth = Math.abs(distance.length() - sumRadii) * 0.5f;
+        Vector2f normal = new Vector2f(distance);
+        normal.normalize();
+        float distanceToPoint = a.getRadius() - depth;
+        Vector2f contactPoint = new Vector2f(a.getCenter()).add(
+                new Vector2f(normal).mul(distanceToPoint));
+
+        result = new CollisionManifold(normal, depth);
+        result.addContactPoint(contactPoint);
+        return result;
+    }
+}
